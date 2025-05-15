ab40817217d3e7d9ae1ef4120bd34e9dfc6c705a
Camila <ca.aceituno@duocuc.cl>
2025-05-15 14:37:33 -0400

    Commit inicial proyecto base

---
+++ src/main/java/com/example/PerfumeriaSPA/PerfumeriaSpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.PerfumeriaSPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeriaSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeriaSpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PerfumeriaSPA/PerfumeriaSpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PerfumeriaSPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeriaSpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

46cadbbe7966b08593f6e068acbc693f1d662d2a
Camila Aceituno <ca.aceituno@duocuc.cl>
2025-05-26 21:53:49 -0400

    Arreglos Controller

--- src/main/java/com/example/perfulandiaspa/Config/RestTemplateConfig.java
+++ src/main/java/com/example/perfulandiaspa/Config/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.example.perfulandiaspa.Config;
+package com.example.perfulandiaspa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
